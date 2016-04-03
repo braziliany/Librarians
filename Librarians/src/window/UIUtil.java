b05694aada17041693fc6cb48114b2eee222738b
murphy <murphy@appsolution.mshome.net>
2016-04-03 13:59:34 +0800

    第一次

---
+++ Librarians/src/window/UIUtil.java
@@ -0,0 +1,21 @@
+package window;
+
+import java.util.Enumeration;
+
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
+
+public class UIUtil {
+	public static void setUIFont(FontUIResource f) {
+		@SuppressWarnings("rawtypes")
+		Enumeration keys = UIManager.getDefaults().keys();
+		while (keys.hasMoreElements()) {
+			Object key = keys.nextElement();
+			Object value = UIManager.get(key);
+			
+			if (value instanceof FontUIResource) {
+				UIManager.put(key, f);
+			}
+		}
+	}
+}
